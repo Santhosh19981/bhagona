63112e0785e5fb21eb01ac9ef31e41d58bd822f2
santosh kumar <santosh.developer98@gmail.com>
2025-06-20 19:57:21 +0530

    new changes

--- src/app/components/orderconfirm/orderconfirm.page.ts
+++ src/app/components/orderconfirm/orderconfirm.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-orderconfirm',
@@ -8,10 +9,16 @@ import { Router } from '@angular/router';
   standalone:false,
 })
 export class OrderconfirmPage implements OnInit {
-
+ private platform = inject(Platform);
+ isMobile:boolean = false;
   constructor(private router: Router) { }
 
   ngOnInit() {
+     this.isMobile = this.platform.is('cordova') || this.platform.is('capacitor') || this.platform.is('mobile') || this.isMobileBrowser();
+  }
+  isMobileBrowser(): boolean {
+    const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
+    return /android|iphone|ipad|mobile/i.test(userAgent);
   }
   gotoHome(){
     this.router.navigate(['/websitehome']);
